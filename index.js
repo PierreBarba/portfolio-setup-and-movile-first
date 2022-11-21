cab07755a855676cd1f430212fac72562c049231
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2022-11-21 17:25:02 -0600

    change html, css and js.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const menuBtn = document.querySelector('.hamburger');
 const headerNavMobile = document.querySelector('.header-nav-mobile');
-const btnCloseNav = document.querySelector('.btn-close-nav');
+const cancelCross = document.querySelector('.cancel-cross');
+const linkSection =document.getElementsByClassName('menu-link');
 
-menuBtn.addEventListener('click', () => {
-    headerNavMobile.classList.remove('hidden')
-});
 
-btnCloseNav.addEventListener('click', () => {
-    menuBtn.classList.remove('hidden');
-    headerNavMobile.classList.add('hidden');
-})
+
+
 
