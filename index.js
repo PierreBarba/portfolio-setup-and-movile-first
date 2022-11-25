3737138e1303b04f9f9294ba54c53ea32fb1e8a6
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2022-11-25 09:20:03 -0600

    Linter install and fix

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
 const projects = [
-    {
-        url:'./imgdesktopPopUp.svg',
-        key: 'project1',
-      titulo: 'Multi-Post Stories Gain+Glory',
-      tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
-      btnLiveHref:'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
-      btnSourceHref:'https://github.com/PierreBarba',
-      imagen: './crossimg.svg',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!'
+  {
+    url: './imgdesktopPopUp.svg',
+    key: 'project1',
+    titulo: 'Multi-Post Stories Gain+Glory',
+    tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnLiveHref: 'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
+    btnSourceHref: 'https://github.com/PierreBarba',
+    imagen: './crossimg.svg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!',
 
-    },
-    {
-        key: 'project2',
-      titulo: 'Multi-Post Stories Gain+Glory',
-      tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
-      btnLiveHref:'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
-      btnSourceHref:'https://github.com/PierreBarba',
-      imagen: './crossimg.svg',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!'
+  },
+  {
+    key: 'project2',
+    titulo: 'Multi-Post Stories Gain+Glory',
+    tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnLiveHref: 'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
+    btnSourceHref: 'https://github.com/PierreBarba',
+    imagen: './crossimg.svg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!',
 
-    },
-    {
-        key: 'project3',
-      titulo: 'Multi-Post Stories Gain+Glory',
-      tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
-      btnLiveHref:'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
-      btnSourceHref:'https://github.com/PierreBarba',
-      imagen: './crossimg.svg',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!'
-    },
-    {
-        key: 'project4',
-      titulo: 'Multi-Post Stories Gain+Glory',
-      tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
-      btnLiveHref:'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
-      btnSourceHref:'https://github.com/PierreBarba',
-      imagen: './crossimg.svg',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!'
-    },
-    {
-        key: 'project5',
-      titulo: 'Multi-Post Stories Gain+Glory',
-      tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
-      btnLiveHref:'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
-      btnSourceHref:'https://github.com/PierreBarba',
-      imagen: './crossimg.svg',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!'
-    },
-    {
-        key: 'project6',
-      titulo: 'Multi-Post Stories Gain+Glory',
-      tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
-      imagen: './crossimg.svg',
-      btnLiveHref:'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
-      btnSourceHref:'https://github.com/PierreBarba',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!'
-    },
-  ];
+  },
+  {
+    key: 'project3',
+    titulo: 'Multi-Post Stories Gain+Glory',
+    tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnLiveHref: 'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
+    btnSourceHref: 'https://github.com/PierreBarba',
+    imagen: './crossimg.svg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!',
+  },
+  {
+    key: 'project4',
+    titulo: 'Multi-Post Stories Gain+Glory',
+    tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnLiveHref: 'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
+    btnSourceHref: 'https://github.com/PierreBarba',
+    imagen: './crossimg.svg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!',
+  },
+  {
+    key: 'project5',
+    titulo: 'Multi-Post Stories Gain+Glory',
+    tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    btnLiveHref: 'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
+    btnSourceHref: 'https://github.com/PierreBarba',
+    imagen: './crossimg.svg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!',
+  },
+  {
+    key: 'project6',
+    titulo: 'Multi-Post Stories Gain+Glory',
+    tecnologias: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+    imagen: './crossimg.svg',
+    btnLiveHref: 'https://pierrebarba.github.io/portfolio-setup-and-movile-first/',
+    btnSourceHref: 'https://github.com/PierreBarba',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga! Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad molestiae dolorem excepturi ipsam dolorum illum molestias vero veritatis fuga!',
+  },
+];
 
-  function getProject(projectKey) {
-    for (let i=0; i< projects.length; i+=1) {
-        if (projects[i].key === projectKey) {
-            return projects[i];
-        }
+function getProject(projectKey) {
+  for (let i = 0; i < projects.length; i += 1) {
+    if (projects[i].key === projectKey) {
+      return projects[i];
     }
-    return false;
   }
-    
-   function createModal (project) { 
-     const projectData =getProject(project);
-    const modalPopUp = `
+  return false;
+}
+
+function createModal(project) {
+  const projectData = getProject(project);
+  const modalPopUp = `
   <article class="card-work-details-mobile show" id="card">
   <button type="button" class="btn-close">
     <img class="closePng" src="./closeButton.png" onclick=hideModal()>
@@ -85,9 +85,7 @@ const projects = [
       <div class="informationCard">
           <div  class = "cardTag"></div>
               <ul>
-                  ${ projectData.tecnologias.map((tec) => {
-                   return `<li class="myRecentWorkItem">${tec}</li>`
-                  }).join('\n')}
+                  ${projectData.tecnologias.map((tec) => `<li class="myRecentWorkItem">${tec}</li>`).join('\n')}
               </ul>
           </div>
       </div>
@@ -107,95 +105,94 @@ const projects = [
   </section>
 </article>            
   `;
-    document.querySelector('.modal').innerHTML= modalPopUp
-                  return modalPopUp
-   }
-
-  // cardeElement es solo el nombre que le di para el objeto que estoy
-  // creando en este caso es la tarjeta
-  const createcard = (project) => {
-    //const cardModal = createModal(project)
-  //creando contenedor de la tarjeta
-    const cardElement = document.createElement('section');
-    // asignando propiedades del contenedor cardElement (class,id)
-    cardElement.classList.add('post');
-    // creando titulo de la tarjeta
-    const cardSection = document.createElement('section');
-    cardSection.classList.add('cardInformation');
-  
-    const cardTitle = document.createElement('h3');
-    cardTitle.classList.add('subtitle');
-    cardTitle.innerText = project.titulo;
-    // creando tecnologias
-    const ulCard = document.createElement('ul');
-    // creando hijo de tecnologías (li)
-  
-    project.tecnologias.forEach((tec) => {
-      const listItem = document.createElement('li');
-      listItem.classList.add('myRecentWorkItem');
-      listItem.innerText = tec;
-      ulCard.appendChild(listItem);
-    });
-    
-    //   // creando boton
-    const buttonCardEnd = document.createElement('button');
-    buttonCardEnd.type = 'button';
-    buttonCardEnd.classList.add('buttongreen');
-    buttonCardEnd.innerText = 'See Project';
-    buttonCardEnd.setAttribute('onclick', `createModal('${project.key}')`);
-  
-    cardSection.appendChild(cardTitle);
-    cardSection.appendChild(ulCard);
-    cardSection.appendChild(buttonCardEnd);
-  
-    cardElement.appendChild(cardSection);
-    return cardElement;
-
-   };
-
-   function hideModal (){
-    const modal = document.getElementById('card');
-    modal.classList.toggle('show')
-   
+  document.querySelector('.modal').innerHTML = modalPopUp;
+  return modalPopUp;
 }
+console.log(createModal);
+// cardeElement es solo el nombre que le di para el objeto que estoy
+// creando en este caso es la tarjeta
+const createcard = (project) => {
+  // const cardModal = createModal(project)
+  // creando contenedor de la tarjeta
+  const cardElement = document.createElement('section');
+  // asignando propiedades del contenedor cardElement (class,id)
+  cardElement.classList.add('post');
+  // creando titulo de la tarjeta
+  const cardSection = document.createElement('section');
+  cardSection.classList.add('cardInformation');
+
+  const cardTitle = document.createElement('h3');
+  cardTitle.classList.add('subtitle');
+  cardTitle.innerText = project.titulo;
+  // creando tecnologias
+  const ulCard = document.createElement('ul');
+  // creando hijo de tecnologías (li)
+
+  project.tecnologias.forEach((tec) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('myRecentWorkItem');
+    listItem.innerText = tec;
+    ulCard.appendChild(listItem);
+  });
+
+  //   // creando boton
+  const buttonCardEnd = document.createElement('button');
+  buttonCardEnd.type = 'button';
+  buttonCardEnd.classList.add('buttongreen');
+  buttonCardEnd.innerText = 'See Project';
+  buttonCardEnd.setAttribute('onclick', `createModal('${project.key}')`);
+
+  cardSection.appendChild(cardTitle);
+  cardSection.appendChild(ulCard);
+  cardSection.appendChild(buttonCardEnd);
+
+  cardElement.appendChild(cardSection);
+  return cardElement;
+};
+
+function hideModal() {
+  const modal = document.getElementById('card');
+  modal.classList.toggle('show');
+}
+console.log(hideModal);
 //   // llama la funcion y la ejecuta
-  
-  // insert cards into cardsGird : iterar arreglo de proyectos e insertar tarjetas
-  
-  const insertCards = () => {
-    const cardGridContainer = document.createElement('div');
-    cardGridContainer.classList.add('cardGrid');
-    projects.forEach((project) => {
-      cardGridContainer.appendChild(createcard(project));
-    });
-    document.getElementById('myRecentWork').appendChild(cardGridContainer);
-  };
-  
-  // insertCards();
-  
-  const menuBtn = document.querySelector('.hamburger');
-  const headerNavMobile = document.querySelector('.header-nav-mobile');
-  const cancelCross = document.querySelector('.cancel-cross');
-  const linkSection = document.getElementsByClassName('menu-link');
-  
-  menuBtn.addEventListener('click', () => {
-    headerNavMobile.classList.remove('hidden');
+
+// insert cards into cardsGird : iterar arreglo de proyectos e insertar tarjetas
+
+const insertCards = () => {
+  const cardGridContainer = document.createElement('div');
+  cardGridContainer.classList.add('cardGrid');
+  projects.forEach((project) => {
+    cardGridContainer.appendChild(createcard(project));
   });
-  
-  cancelCross.addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden');
-  });
-  
-  linkSection[0].addEventListener('click', () => {
-    headerNavMobile.classList.toggle('hidden');
-  });
-  
-  linkSection[1].addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden');
-  });
-  
-  linkSection[2].addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden');
-  });
-  
-  document.addEventListener('DOMContentLoaded', insertCards);
+  document.getElementById('myRecentWork').appendChild(cardGridContainer);
+};
+
+// insertCards();
+
+const menuBtn = document.querySelector('.hamburger');
+const headerNavMobile = document.querySelector('.header-nav-mobile');
+const cancelCross = document.querySelector('.cancel-cross');
+const linkSection = document.getElementsByClassName('menu-link');
+
+menuBtn.addEventListener('click', () => {
+  headerNavMobile.classList.remove('hidden');
+});
+
+cancelCross.addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+});
+
+linkSection[0].addEventListener('click', () => {
+  headerNavMobile.classList.toggle('hidden');
+});
+
+linkSection[1].addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+});
+
+linkSection[2].addEventListener('click', () => {
+  headerNavMobile.classList.add('hidden');
+});
+
+document.addEventListener('DOMContentLoaded', insertCards);
